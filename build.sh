356f54e2754101bd72064b303d426ae7cfa8eef5
Roni Koitermaa <roninkoi@iki.fi>
2021-11-27 10:05:38 +0200

    Fix build

--- build.sh
+++ build.sh
@@ -8,12 +8,12 @@ if [ $# == 1 ]; then
 fi
 
 if [ $render == 0 ]; then
-    sed -i 's/#define RENDER_ENABLED.*/#define RENDER_ENABLED 0/p' src/sim.h
+    sed -i 's/#define RENDER_ENABLED.*/#define RENDER_ENABLED 0/g' src/sim.h
     cmake -DRENDER=OFF .
 fi
 
 if [ $render == 1 ]; then
-    sed -i 's/#define RENDER_ENABLED.*/#define RENDER_ENABLED 1/p' src/sim.h
+    sed -i 's/#define RENDER_ENABLED.*/#define RENDER_ENABLED 1/g' src/sim.h
     cmake -DRENDER=ON .
 fi
 
